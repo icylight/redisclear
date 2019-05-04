d9d6c7cffcbe6604f88bd98da1545f1dd703c266
icylight <408209435@qq.com>
2019-05-04 15:59:00 +0800

    更新 main.go

--- main.go
+++ main.go
@@ -35,25 +35,25 @@ func main() {
 	}
 	keys, err := redis.Keys("live-service:im:room:*").Result()
 
-	fmt.Println("keys:")
-	for i := 0; i < len(keys); i++ {
-		fmt.Println(keys[i])
-	}
+	fmt.Printf("key count: %d\n", len(keys))
 	fmt.Print("\n")
 	fmt.Println("start clear redis")
 
 	score := int64(1556913600)
+	count := int64(0)
 	fmt.Printf("clear time: %s\n", time.Unix(score, 0))
 
 	for i := 0; i < len(keys); i++ {
-		_, err := redis.ZRemRangeByScore(keys[i], "-inf", strconv.FormatInt(score, 10)).Result()
+		fmt.Println(keys[i])
+		n, err := redis.ZRemRangeByScore(keys[i], "-inf", strconv.FormatInt(score, 10)).Result()
 		if err != nil {
 			fmt.Println(err)
 			fmt.Println("clear failed")
 			os.Exit(1)
 		}
+		count += n
 	}
 
-	fmt.Println("\nfinish")
+	fmt.Printf("\nfinish count: %d\n", count)
 	return
 }
